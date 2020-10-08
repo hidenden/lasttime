b7f79a5ec64408070e36ac86da511e5411cb1c9c
Hidenari Miwa <miwa.hidenari@jp.fujitsu.com>
2020-10-08 11:20:02 +0900

    Version flag available

--- cmd/lasttime/main.go
+++ cmd/lasttime/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"runtime"
 
@@ -9,8 +10,25 @@ import (
 )
 
 var revision string
+var version string
+
+func parseOption() bool {
+	var optVersion bool
+	flag.BoolVar(&optVersion, "v", false, "show version")
+	flag.BoolVar(&optVersion, "version", false, "show version")
+	flag.Parse()
+	if optVersion {
+		fmt.Printf("version:%v(%v)", version, revision)
+		return false
+	}
+	return true
+}
 
 func main() {
+	if !parseOption() {
+		return
+	}
+
 	fmt.Println("Hello World")
 	fmt.Println("Number of Go routine", runtime.NumGoroutine())
 	fmt.Println("Command revision", revision)
